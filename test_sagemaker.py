c3e0c9ffa50c3aaa57681d57ce09c11b7a5c740a
EC2 Default User <ec2-user@ip-172-31-7-251.ec2.internal>
2018-03-07 17:37:47 +0000

    signature tests and lots

--- test_sagemaker.py
+++ test_sagemaker.py
@@ -2,11 +2,12 @@ import unittest
 import os
 import shutil
 import json
+import inspect
 
 import mxnet as mx
 
-import model as model
-import model_io as io
+import model_cifar as model
+import model_cifar_io as io
 
 # import example_model as model
 
@@ -15,6 +16,7 @@ class MXNetModelTest(unittest.TestCase):
     
     @classmethod
     def setUpClass(self):
+        
         self.bucket_path = "bucket"
         self.channel_input_dirs = {'train': os.path.join(self.bucket_path, 'dataset/train'),
                              'eval': os.path.join(self.bucket_path, 'dataset/eval')}
@@ -24,29 +26,51 @@ class MXNetModelTest(unittest.TestCase):
         os.makedirs(self.bucket_path)
         os.makedirs(self.model_dir)
         
-        hyperparameters = {'learningRate': '0.01', 'epochs': 10}
-        num_gpus = 1
-        num_cpus = 0
-        kwargs = {'key': 'value'}
-
-        self.model = model.train(hyperparameters=hyperparameters,
-                                    channel_input_dirs=self.channel_input_dirs,
-                                    output_data_dir=self.output_data_dir,
-                                    model_dir=self.model_dir,
-                                    num_gpus=num_gpus,
-                                    num_cpus=num_cpus,
-                                    **kwargs)
+        if 'train' in dir(model):
+            hyperparameters = {'learningRate': '0.01', 'epochs': 1}
+            num_gpus = 1
+            num_cpus = 0
+            kwargs = {'key': 'value'}
+            
+            self.model = model.train(hyperparameters=hyperparameters,
+                                        channel_input_dirs=self.channel_input_dirs,
+                                        output_data_dir=self.output_data_dir,
+                                        model_dir=self.model_dir,
+                                        num_gpus=num_gpus,
+                                        num_cpus=num_cpus,
+                                        **kwargs)
     
     @classmethod
     def tearDownClass(self):
         shutil.rmtree('bucket')
         pass
+    
+    # ---------------------------------------------------------------------------- #
+    # Test signatures                                                           #
+    # ---------------------------------------------------------------------------- #
+      
+    def test_1_signature(self):
+        if 'save' in dir(model):
+            self.assertEqual(inspect.getargspec(model.save).args, ["model", "model_dir"])
         
+        if 'model_fn' in dir(model):
+             self.assertEqual(inspect.getargspec(model.model_fn).args, ["model_dir"])
+        if 'transform_fn' in dir(model):
+            self.assertEqual(inspect.getargspec(model.transform_fn).args, ["model", "input_data", "content_type", "accept"])
+            
+        if 'input_fn' in dir(model):
+            self.assertEqual(inspect.getargspec(model.input_fn).args, ["input_data", "content_type"])
+        if 'predict_fn' in dir(model):
+            self.assertEqual(inspect.getargspec(model.predict_fn).args, ["block", "array"])
+        if 'output_fn' in dir(model):
+            self.assertEqual(inspect.getargspec(model.output_fn).args, ["ndarray", "accept"])
+     
+     
     # ---------------------------------------------------------------------------- #
     # Training functions                                                           #
     # ---------------------------------------------------------------------------- #
 
-    def test_1_save(self):
+    def test_2_save(self):
         """
         [Optional]
         
@@ -56,20 +80,10 @@ class MXNetModelTest(unittest.TestCase):
             self.assertIsNotNone(self.model, "save function defined but not used")
             model.save(model=self.model, model_dir=self.model_dir)
 
+
     # ---------------------------------------------------------------------------- #
     # Hosting functions                                                            #
     # ---------------------------------------------------------------------------- #
-
-    def test_2_model_fn(self):
-        """
-        [Optional]
-
-        Test model_fn function.
-        """
-        if 'model_fn' in dir(model):
-            self.assertIsNotNone(self.model, "model_fn function defined but not used")
-            model.model_fn(self.model_dir)
-
 
     def test_3_transform_fn(self):
         """
@@ -78,45 +92,26 @@ class MXNetModelTest(unittest.TestCase):
         Test transform_fn function.
         """
         if 'transform_fn' in dir(model):
-            self.assertIsNotNone(self.model, "transform_fn function defined but not used")
-            response_body, accept = model.transform_fn(model=self.model,
+            self.assertTrue('model_fn' in dir(model), 'Currently requires model_fn to test transform_fn')
+            net = model.model_fn(self.model_dir)
+            response_body, accept = model.transform_fn(model=net,
                                                         input_data=io.input_data,
                                                         content_type=io.content_type,
                                                         accept=io.accept)
             self.assertEqual(response_body, io.response_body)
+            self.assertEqual(accept, io.accept)
             
-
 
     # ---------------------------------------------------------------------------- #
     # Request handlers for Gluon models                                            #
     # ---------------------------------------------------------------------------- #
 
-    def test_input_fn(self):
+    def test_4_input_predict_output_fn(self):
         """
         [Optional]
 
         Test input_fn function.
         """
-        # if 'input_fn' in dir(model):
-        #     return_value = model.input_fn(input_data="", content_type="text/json")
-        #     self.assertIsInstance(return_value, mx.nd.NDArray)
-
-    def test_predict_fn(self):
-        """
-        [Optional]
-
-        Test predict_fn function.
-        """
-        # if 'predict_fn' in dir(model):
-        #     return_value = model.predict_fn(block="", array=mx.nd.array([1, 2, 3]))
-        #     self.assertIsInstance(return_value, mx.nd.NDArray)
-
-    def test_output_fn(self):
-        """
-        [Optional]
-
-        Test output_fn function.
-        """
-        # if 'output_fn' in dir(model):
-        #     return_value = model.output_fn(ndarray=mx.nd.array([1, 2, 3]), accept="text/json")
-        #     self.assertIsInstance(return_value, tuple)
+        if 'input_fn' in dir(model):
+            self.assertTrue('model_fn' in dir(model), 'Currently requires model_fn to test transform_fn')
+            net = model.model_fn(self.model_dir)
